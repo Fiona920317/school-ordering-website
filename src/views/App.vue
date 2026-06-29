29b286615e5f751f97455a0682490d057197f222
Fiona920317 <fiona24826455@gmail.com>
2026-06-29 15:23:47 +0800

    測試
    試試看怎麼用github desktop更新

--- src/views/App.vue
+++ src/views/App.vue
@@ -3,5 +3,5 @@ import HelloWorld from "../components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="bg-secondary text-primary">12345</div>
+  <div class="bg-secondary text-primary">你好我是Vue</div>
 </template>
